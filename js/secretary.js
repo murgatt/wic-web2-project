5e16a8af58a4847d3856d2b003da45c85cea9428
murgatt <theomurgat@hotmail.fr>
2016-04-29 01:04:35 +0200

    Edit & Delete patient feature

--- js/secretary.js
+++ js/secretary.js
@@ -6,16 +6,12 @@ require("angular-material/angular-material.css");
 var ngMessages = require('angular-messages')
 
 var cabinetModule = angular.module("cabinet", [angularMaterial, 'ngMessages'])
-    .config(function ($mdDateLocaleProvider) {
-        // Example of a French localization.
+    .config(function ($mdDateLocaleProvider) { // Configurer les valeurs en français pour le calendrier
         $mdDateLocaleProvider.months = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'];
         $mdDateLocaleProvider.shortMonths = ['janv', 'févr', 'mars', 'avr', 'mai', 'juin', 'juil', 'août', 'sept', 'oct', 'nov', 'déc'];
         $mdDateLocaleProvider.days = ['dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi'];
         $mdDateLocaleProvider.shortDays = ['Di', 'Lu', 'Ma', 'Me', 'Je', 'Ve', 'Sa'];
-        // Can change week display to start on Monday.
         $mdDateLocaleProvider.firstDayOfWeek = 1;
-        // In addition to date display, date components also need localized messages
-        // for aria-labels for screen-reader users.
         $mdDateLocaleProvider.weekNumberFormatter = function (weekNumber) {
             return 'Semaine ' + weekNumber;
         };
